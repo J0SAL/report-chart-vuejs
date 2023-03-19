d30667a5bf46e4a7defa9ce832b48d4ca6a9c35e
J0SAL <joyalmeida321@gmail.com>
2023-03-19 23:51:06 +0530

    updated package.json

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LineChart />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LineChart from './components/LineChart'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LineChart
   }
 }
 </script>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,55 @@
+<template>
+    <Line
+      id="my-chart-id"
+      :options="chartOptions"
+      :data="chartData"
+      v-if="loaded"
+    />
+  </template>
+  
+  <script>
+  import { Line } from 'vue-chartjs'
+  import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend 
+  } from 'chart.js'
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+    )
+  
+  export default {
+    name: 'LineChart',
+    components: { Line },
+    data() {
+      return {
+        chartData: {
+            labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            datasets: [
+                {
+                    label: "Google Transparency Report",
+                    backgroundColor: "#f87979",
+                    data: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 10, 20] 
+                } 
+            ]
+        },
+        chartOptions: {
+          responsive: true,
+          maintainAspectRatio: false
+        }
+      }
+    }
+  }
+  </script>
